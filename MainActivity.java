38f99b679d2265e9b4decfa9a35c23b910d8eaf0
srivathsan-hash <84728001+srivathsan-hash@users.noreply.github.com>
2022-03-06 23:49:18 -0800

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -8,43 +8,44 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<font_size> extends AppCompatActivity {
     Button bn1;
     Button bn2;
     Button bn3;
+    Button button;
     TextView display;
     Toast t;
+    int font_size;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bn1 = (Button) findViewById(R.id.bn1);
-        bn2 = (Button) findViewById(R.id.bn2);
-        bn3 = (Button) findViewById(R.id.bn3);
-        display = (TextView) findViewById(R.id.textView);
+        bn1 = findViewById(R.id.bn1);
+        bn2 = findViewById(R.id.bn2);
+        bn3 = findViewById(R.id.bn3);
+        button= findViewById(R.id.button);
+
+        display = findViewById(R.id.textView);
         t = Toast.makeText(this,"buttons are working",Toast.LENGTH_LONG);
-        bn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                display.setTextSize(50);
+        bn1.setOnClickListener(view -> {
+                font_size+=40;
+                display.setTextSize(font_size);
                 t.show();
 
-            }
         });
-        bn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                display.setTextColor(Color.GREEN);
-                t.show();
-            }
+        button.setOnClickListener(view -> {
+            font_size-=40;
+            display.setTextSize(font_size);
+            t.show();
         });
-        bn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            bn2.setOnClickListener(view -> {
+            display.setTextColor(Color.GREEN);
+            t.show();
+        });
+        bn3.setOnClickListener(view -> {
 
-                display.setBackgroundColor(Color.RED);
-                t.show();
-            }
+            display.setBackgroundColor(Color.RED);
+            t.show();
         });
 
 
